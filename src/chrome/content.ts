1e4df42e5fcf81aae5236b9efb3f9d8efd967659
manapixels <chrenelias@outlook.com>
2022-05-13 19:58:39 +0800

    Add fixed frames in all tabs

--- src/chrome/content.ts
+++ src/chrome/content.ts
@@ -22,5 +22,18 @@ const main = () => {
     * Fired when a message is sent from either an extension process or a content script.
     */
    chrome.runtime.onMessage.addListener(messagesFromReactAppListener)
+
 }
 main()
+
+var extensionOrigin = 'chrome-extension://' + chrome.runtime.id;
+if (!window.location.ancestorOrigins.contains(extensionOrigin)) {
+    var iframe = document.createElement('iframe');
+    // Must be declared at web_accessible_resources in manifest.json
+    iframe.src = chrome.runtime.getURL('modal.html');
+
+    // Some styles for a fancy sidebar
+    iframe.style.cssText = 'position:fixed;top:0;right:0;display:block;' +
+                           'width:300px;height:100%;z-index:1000;';
+    document.body.appendChild(iframe);
+}
