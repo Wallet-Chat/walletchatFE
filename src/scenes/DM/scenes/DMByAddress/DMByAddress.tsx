877fa5ebe8f20c8efab2f9e305f4b80a081b86fd
Cyber Kevin <emailkevinlarson@gmail.com>
2022-12-15 09:56:45 -0700

    workaround for PFP data glitches
    need to move to redux

--- src/scenes/DM/scenes/DMByAddress/DMByAddress.tsx
+++ src/scenes/DM/scenes/DMByAddress/DMByAddress.tsx
@@ -101,8 +101,11 @@ const DMByAddress = ({
          })
             .then((response) => response.json())
             .then((response) => {
-               console.log('✅[GET][Image FromAddr]:', response)
-               if (response[0]?.base64data) setPfpDataFromAddr(response[0].base64data)
+               console.log('✅[GET][Image FromAddr]:', account, response)
+               if (response[0]?.base64data) {
+                  setPfpDataFromAddr(response[0].base64data)
+                  localStorage['pfpData_' + account] = response[0].base64data
+               }
                else {
                   setPfpDataFromAddr('')
                   console.log('cleared from PFP')
@@ -114,26 +117,31 @@ const DMByAddress = ({
       }
 
       if (toAddr) {
-         // fetch(` ${process.env.REACT_APP_REST_API}/${process.env.REACT_APP_API_VERSION}/image/${toAddr}`, {
-         //    method: 'GET',
-         //    credentials: "include",
-         //    headers: {
-         //       'Content-Type': 'application/json',
-         //       Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-         //    },
-         // })
-         //    .then((response) => response.json())
-         //    .then((response) => {
-         //       console.log('✅[GET][Image]:', response)
-         //       if (response[0]?.base64data) setPfpDataToAddr(response[0].base64data)
-         //       else {
-         //          setPfpDataToAddr('')
-         //          console.log('cleared to PFP')
-         //       }
-         //    })
-         //    .catch((error) => {
-         //       console.error('🚨[GET][Image]:', error)
-         //    })
+         if (localStorage.getItem("'pfpData_' + account") === null) {
+            fetch(` ${process.env.REACT_APP_REST_API}/${process.env.REACT_APP_API_VERSION}/image/${toAddr}`, {
+               method: 'GET',
+               credentials: "include",
+               headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: `Bearer ${localStorage.getItem('jwt')}`,
+               },
+            })
+               .then((response) => response.json())
+               .then((response) => {
+                  console.log('✅[GET][Image ToAddr]:', toAddr, response)
+                  if (response[0]?.base64data) {
+                     setPfpDataToAddr(response[0].base64data)
+                     localStorage['pfpData_' + toAddr] = response[0].base64data
+                  }
+                  else {
+                     setPfpDataToAddr('')
+                     console.log('cleared to PFP')
+                  }
+               })
+               .catch((error) => {
+                  console.error('🚨[GET][Image]:', error)
+               })
+         }
 
          //load chat data from localStorage to chatData
          setChatData(localStorage["dmData_" + account + "_" + toAddr.toLowerCase()] ? JSON.parse(localStorage["dmData_" + account + "_" + toAddr.toLowerCase()]) : [])
@@ -189,29 +197,7 @@ const DMByAddress = ({
          setIsFetchingChatData(false)
          const temp = [] as MessageUIType[]
          setLoadedMsgs(temp)
-         setPfpDataToAddr('')   
-         if (toAddr) {
-            fetch(` ${process.env.REACT_APP_REST_API}/${process.env.REACT_APP_API_VERSION}/image/${toAddr}`, {
-               method: 'GET',
-               credentials: "include",
-               headers: {
-                  'Content-Type': 'application/json',
-                  Authorization: `Bearer ${localStorage.getItem('jwt')}`,
-               },
-            })
-               .then((response) => response.json())
-               .then((response) => {
-                  console.log('✅[GET][Image]:', response)
-                  if (response[0]?.base64data) setPfpDataToAddr(response[0].base64data)
-                  else {
-                     setPfpDataToAddr('')
-                     console.log('cleared to PFP')
-                  }
-               })
-               .catch((error) => {
-                  console.error('🚨[GET][Image]:', error)
-               })
-            }
+         setPfpDataToAddr('')
          return //skip the account transition glitch
       }
       setPrevAddr(toAddr)
@@ -637,7 +623,7 @@ const DMByAddress = ({
                   <Flex alignItems="center">
                   {pfpDataToAddr ? (
                            <Image
-                              src={pfpDataToAddr}
+                              src={localStorage['pfpData_' + toAddr.toLocaleLowerCase()]}
                               height="40px"
                               width="40px"
                               borderRadius="var(--chakra-radii-xl)"
@@ -725,7 +711,7 @@ const DMByAddress = ({
                      context="dms"
                      account={account}
                      msg={msg}
-                     pfpImage={pfpDataToAddr}
+                     pfpImage={localStorage['pfpData_' + msg.fromAddr]}
                      updateRead={updateRead}
                   />
                )
