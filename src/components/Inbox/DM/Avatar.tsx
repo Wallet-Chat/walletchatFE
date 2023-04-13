42940ef64f4ac9356081dd42b941e9e33f9fb793
Rafael Saes <git@saes.io>
2023-04-12 21:11:16 -0300

    feat: make all avatars fit in

--- src/components/Inbox/DM/Avatar.tsx
+++ src/components/Inbox/DM/Avatar.tsx
@@ -1,32 +1,39 @@
 import React from 'react'
 import Blockies from 'react-blockies'
-import { Image } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
 import { useGetPfpQuery } from '@/redux/reducers/dm'
 import { BlockieWrapper } from '@/styled/BlockieWrapper'
 
 const AvatarWrapper = ({ children }: { children: React.ReactNode }) => (
-	<BlockieWrapper height='40px' width='40px'>
-		{children}
-	</BlockieWrapper>
+  <BlockieWrapper height='40px' width='40px'>
+    {children}
+  </BlockieWrapper>
 )
 
 function Avatar({ account }: { account: string }) {
-	const accountStr = account.toLocaleLowerCase()
-	const { data: toAddrPfp } = useGetPfpQuery(accountStr)
+  const accountStr = account.toLocaleLowerCase()
+  const { data: toAddrPfp } = useGetPfpQuery(accountStr)
 
-	if (toAddrPfp) {
-		return (
-			<AvatarWrapper>
-				<Image src={toAddrPfp} />
-			</AvatarWrapper>
-		)
-	}
+  if (toAddrPfp) {
+    return (
+      <AvatarWrapper>
+        <Box
+          backgroundImage={`url(${toAddrPfp})`}
+          backgroundSize='cover'
+          backgroundPosition='center'
+          backgroundRepeat='no-repeat'
+          height='40px'
+          width='40px'
+        />
+      </AvatarWrapper>
+    )
+  }
 
-	return (
-		<AvatarWrapper>
-			<Blockies seed={accountStr} scale={5} />
-		</AvatarWrapper>
-	)
+  return (
+    <AvatarWrapper>
+      <Blockies seed={accountStr} scale={5} />
+    </AvatarWrapper>
+  )
 }
 
 export default Avatar
