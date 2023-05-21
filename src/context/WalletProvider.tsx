d98de5576d861e8a290d5d4efd2229ed3c7eb1c6
Cyber Kevin <emailkevinlarson@gmail.com>
2023-05-21 09:29:19 -0700

    remove print

--- src/context/WalletProvider.tsx
+++ src/context/WalletProvider.tsx
@@ -356,8 +356,6 @@ const WalletProviderContext = (chains: any) => {
     const eventListener = async (e: MessageEvent) => {
       const { data }: { data: API } = e
 
-      console.log("*** eventListenr ***", data)
-
       // TODO - KL removed 5/14/2023, seems the ENV vars still update correctly 
       //        can remove this in a bit if no other issues seen.  
       //        This seemed to cause very high CPU usage (maybe infinite loop?)
