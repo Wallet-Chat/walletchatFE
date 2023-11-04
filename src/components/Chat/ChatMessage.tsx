c93330ab5166e6fee9c206a3bc81b5e7516245d0
Cyber Kevin <emailkevinlarson@gmail.com>
2023-11-03 17:43:02 -0700

    update to ensure link stays blocked (updated in localStorage)

--- src/components/Chat/ChatMessage.tsx
+++ src/components/Chat/ChatMessage.tsx
@@ -257,7 +257,7 @@ const ChatMessage = ({
           //console.log("updatedMessageNoBadLinks: ", updatedMessageNoBadLinks)
 
           dispatch(
-            updateQueryChatData({ account, toAddr: fromAddr }, () => {
+              updateQueryChatData({ account, toAddr: fromAddr }, () => {
               let currentChatData: any = (
                 getLocalDmDataForAccountToAddr(account, fromAddr) || []
               )
@@ -285,9 +285,6 @@ const ChatMessage = ({
               return JSON.stringify({ messages: newChatData })
             })
           )
-
-          //WalletGuard API To check for scam links
-          handleScanAndRemoveURL(message);
 
           if (account !== fromAddr) {
             dispatch(
@@ -357,12 +354,13 @@ const ChatMessage = ({
     return messageIn;
   }
 
-  const handleScanAndRemoveURL = (messageIn) => {
+  const handleScanAndRemoveURL = async (messageIn) => {
+    let returnVal = messageIn
     if (message.includes("https") || message.includes("http")) {
 
       const urls = extractUrlsFromMessage(message)
 
-      fetch(
+      await fetch(
         ` ${ENV.REACT_APP_REST_API}/${ENV.REACT_APP_API_VERSION}/wallet_guard_check`,
         {
           method: 'POST',
@@ -376,17 +374,19 @@ const ChatMessage = ({
       )
         .then((response) => response.json())
         .then((recommendedActions: string[]) => {
-          console.log('✅[POST][WalletGuard Link Check]:', recommendedActions)
+          log('✅[POST][WalletGuard Link Check]:', recommendedActions)
 
           const updatedMessage = replaceBlockedUrls(recommendedActions, messageIn) 
-          console.log('Updated Message:', updatedMessage)
+          log('Updated Message:', updatedMessage)
           setMessage(updatedMessage); // Update the local variable
-          
+          returnVal = updatedMessage
         })
         .catch((error) => {
           console.error('🚨[GET][WalletGuard URL Check]:', error)
         })
      }
+
+     return returnVal
   };
 
   return (
